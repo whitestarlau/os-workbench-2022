e689700c4fa94a0c61781adb88a7f62504c09b4b
tracer-nju <tracer@nju.edu.cn>
2024-04-21 08:29:01 +0000

    Linux white 5.15.0-105-generic #115-Ubuntu SMP Mon Apr 15 09:52:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:29:01 up  1:19,  2 users,  load average: 1.32, 1.29, 0.97

--- amgame/src/keyboard.c
+++ amgame/src/keyboard.c
@@ -3,33 +3,42 @@
 #define KEYNAME(key) \
   [AM_KEY_##key] = #key,
 static const char *key_names[] = {
-  AM_KEYS(KEYNAME)
-};
+    AM_KEYS(KEYNAME)};
 
-void print_key() {
-  AM_INPUT_KEYBRD_T event = { .keycode = AM_KEY_NONE };
+void print_key()
+{
+  AM_INPUT_KEYBRD_T event = {.keycode = AM_KEY_NONE};
   ioe_read(AM_INPUT_KEYBRD, &event);
-  if (event.keycode != AM_KEY_NONE && event.keydown) {
+  if (event.keycode != AM_KEY_NONE && event.keydown)
+  {
     puts("Key pressed: ");
     puts(key_names[event.keycode]);
     puts("\n");
   }
 }
 
-
-int wait_exit() {
-  AM_INPUT_KEYBRD_T event = { .keycode = AM_KEY_NONE };
+int wait_exit()
+{
+  AM_INPUT_KEYBRD_T event = {.keycode = AM_KEY_NONE};
   ioe_read(AM_INPUT_KEYBRD, &event);
-  if (event.keycode != AM_KEY_NONE && event.keydown) {
+  puts("wait_exit ");
+
+  if (event.keycode != AM_KEY_NONE && event.keydown)
+  {
     puts("Key pressed: ");
     puts(key_names[event.keycode]);
     puts("\n");
-    if (event.keycode == AM_KEY_ESCAPE) {
-        return 1;
-    } else {
-        return 0;
+    if (event.keycode == AM_KEY_ESCAPE)
+    {
+      return 1;
     }
-  } else {
+    else
+    {
+      return 0;
+    }
+  }
+  else
+  {
     return 0;
   }
 }
